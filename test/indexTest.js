2d2600214ebdbfe8d7b464d9f419a602ce2c1369
estifanos <libsanewb@gmail.com>
2023-03-12 23:55:05 -0400

    assignment is done

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,3 +1,35 @@
+
+
+let employee= {};
+employee.name="Sam";
+employee.streetAddress= "12 Broadway";
+
+
+function  updateEmployeeWithKeyAndValue(employee, key, value){   //
+  const newEmployee = { ...employee };
+  newEmployee[key]=value;
+  return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  employee[key]=value;
+  return employee;
+ }
+
+
+function  deleteFromEmployeeByKey(employee, key){
+  const newEmployee = { ...employee };
+  delete newEmployee.key;
+  //return newEmployee;
+  return employee;
+ }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee.key;
+
+  return employee[key];
+}
+
+
 require ( './helpers.js' );
 
 describe('employees', function() {
